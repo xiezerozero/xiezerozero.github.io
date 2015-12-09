d6b8cbf93c0144c843c2ea0bd412e73dfa77ab05
xiezerozero <814004508@qq.com>
2015-12-09 23:06:30 +0800

    add new index

---
+++ readme/markParse.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author xiezerozero <xielin@qqn.com>
+ */
+
+
+
+include 'Parsedown.php';
+$parser = new Parsedown();
+//源目录
+$sourceDir = 'd:/test_code/xiezerozero.github.io/readme';
+//目标目录
+$targetDir = 'd:/test_code/xiezerozero.github.io';
+
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($sourceDir, FilesystemIterator::SKIP_DOTS),
+    RecursiveIteratorIterator::SELF_FIRST   // lists leaves and parents,parents coming first
+);
+//模板文件内容  替换{{title}}和 {{content}}
+$templateContent = file_get_contents(__DIR__ . '/template.html');
+
+/** @var SplFileInfo $file */
+foreach ($iterator as $file) {
+    //过滤脚本文件
+    if ($file->getExtension() == 'php' || $file->getExtension() == 'html') {
+        continue;
+    }
+    if ($file->isDir()) {
+        // 目标目录名称替换源目录名称   源目录可能是多层级的目录结构
+        $dir = str_replace($sourceDir, $targetDir, $file->getPathname());
+        if (!file_exists($dir)) {
+            mkdir($dir);
+        }
+    } else {
+        // 替换源目录到目标目录,确定生成的html目标文件名称
+        $targetFile = str_replace($sourceDir, $targetDir, $file->getPath()) . '/' . $file->getBasename('.md') . '.html';
+        $source = file_get_contents($file->getPathname());
+        // 获取markdown转html后的内容
+        $parsedContent = $parser->text($source);
+        $content = str_replace(
+            '{{content}}',
+            $parsedContent,
+            str_replace('{{title}}', $file->getBasename('.md'), $templateContent)
+        );
+        file_put_contents($targetFile, $content);
+    }
+}
---
+++ readme/php7/README.md
@@ -0,0 +1,62 @@
+###. Throwable
+    Error
+        ArithmeticError
+        AssertionError
+        DivisionByZeroError
+        ParseError
+        TypeError
+    Exception
+        ....
+        
+        
+###. declare(strict_types=1)  //开启严格模式,不再做类型转换,类型不匹配抛出异常(返回值亦是如此)
+
+function add(int $a, int $b) : int 
+{
+    return $a + $b;
+}
+
+
+###. <=> (太空船运算符) 当$a大于、等于或小于$b时它分别返回-1、0或1(可用作排序)
+
+
+###. ?? null合并运算符
+
+  $userName = $_GET['user'] ?? 'nobody';   
+  // equals
+  $userName = isset($_GET['user']) ? $_GET['user'] : 'nobody';
+  // 多个null合并运算符
+  $username = $_GET['user'] ?? $_POST['user'] ?? 'nobody';
+  
+###. generator
+    
+   function count_to_ten()     //generator implements Iterator
+   {
+       yield 1;
+       yield from [2, 3];
+       return 4;
+   }
+   $a = count_to_ten();
+   var_dump($a);    //object(generator)[1]
+   
+   $a->send($message);
+   $a->getReturn();
+   
+###. 
+   function sum(...$numbers)    //定义不定个数参数(类型最好统一,不然很难处理)
+   {
+       return array_sum($numbers);  //$numbers是所有传递的参数的数组
+   }
+   echo sum(1,2,3,4,5); //$numbers=[1,2,3,4,5];
+   
+   
+   function test(array ...$arrays) : array  //限制每个参数都是array格式,将所有的参数组装到$arrays数组中,申明返回值array类型
+   {
+       return array_map(function ($v) : int {  //匿名函数申明返回值是int(declare严格模式不会隐式转换)
+            return array_sum($v);
+       }, $arrays);
+   }
+   var_dump(test([1, 2.2, 3.5], [4, 5, 6], [7, 8, 9])); //$arrays=[[1, 2.2, 3.5], [4, 5, 6], [7, 8, 9]];
+   
+   
+    
